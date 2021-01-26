2cac4fb30df6e05cd7d62bda779a0bf9b38015a5
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2021-01-26 09:21:53 +0100

    Initial commit

---
+++ TestApp/Data/Collega.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Data
+{
+    public class Collega
+    {
+        public string Naam { get; set; }
+        public string Email { get; set; }
+        public string TelefoonNummer { get; set; }
+
+        public Collega(string naam, string email, string telefoonNummer)
+        {
+            Naam = naam;
+            Email = email;
+            TelefoonNummer = telefoonNummer;
+        }
+    }
+
+}
---
+++ TestApp/Data/CollegaServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Data
+{
+    public class CollegaServices : CollegaInterface
+    {
+        public Task<Collega[]> GetCollega()
+        {
+            List<Collega> collegas = new List<Collega> { new Collega("Rick", "test", "test"), new Collega("Jordy", "test2", "test2") };
+            return Task.FromResult(collegas.ToArray());
+        }
+    }
+}
+
+
+
